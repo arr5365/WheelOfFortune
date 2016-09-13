60e8a000203240df0add8f2188f65e18adcbeed4
arr5365 <arr5365@psu.edu>
2016-09-12 20:14:13 -0400

    Added a loop for user input.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -8,13 +8,36 @@
 */
 package wheeloffortune;
 
+import java.util.Scanner;
+
 public class WheelOfFortune {
 
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    
+      String action;
+      Scanner scanner = new Scanner(System.in);
+      
+     System.out.println("What do you want to do? \nEnter:1 To Spin the Wheel., \nEnter:2 To buy a Vowel., "
+             + "\nEnter:3 To Solve the Puzzle. or \nEnter:4 To Quit.");
+     action = scanner.next();
+     System.out.println(action);
+     if ("1".equals(action)) {
+         action = "You choose to spin the wheel.";
+     }
+     if ("2".equals(action)){ 
+         action = "You choose to buy a vowel.";
+     }
+     if ("3".equals(action)){ 
+         action = "You choose to solve the puzzle.";
+     }
+     if ("4".equals(action)) 
+     {action = "You choose to quit.";
+     return;
+     }
+     System.out.println(action);
   }
+  
   
 }
