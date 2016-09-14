6f3cfdaf96137cf3561f699dfaef367ecd5ed0fb
arr5365 <arr5365@OZWDLNDLIB-231.win.psu.edu>
2016-09-14 12:48:27 -0400

    Added Looping for users action.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -16,27 +16,41 @@ public class WheelOfFortune {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+      action();
+  }
+  
+  public static void action(){
       String action;
+      String letter;
       Scanner scanner = new Scanner(System.in);
       
      System.out.println("What do you want to do? \nEnter:1 To Spin the Wheel., \nEnter:2 To buy a Vowel., "
              + "\nEnter:3 To Solve the Puzzle. \nEnter:4 To Quit.");
      action = scanner.next();
      System.out.println(action);
-     if ("1".equals(action)) {
+     
+     while ((action != "0")){
+         if ("1".equals(action)) {
          action = "You choose to spin the wheel.";
+         System.out.println(action);
+         
      }
-     if ("2".equals(action)){ 
+         if ("2".equals(action)){ 
          action = "You choose to buy a vowel.";
+         System.out.println(action);
      }
-     if ("3".equals(action)){ 
+         if ("3".equals(action)){ 
          action = "You choose to solve the puzzle.";
+         System.out.println(action);
      }
-     if ("4".equals(action)) 
-     {action = "You choose to quit.";
-     return;
+         if ("4".equals(action)) 
+         {action = "You choose to quit.";
+         System.out.println(action);
+         System.exit(0);
      }
-     System.out.println(action);
+         action();
+     }
+      
   }
   
   
